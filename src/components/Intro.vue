368c75d81e8b7850e8629ba89791a6744e449960
blackseriesC6 <blackseries.n63@gmail.com>
2024-06-06 09:40:13 +0500

    about Final

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,46 +1,46 @@
 <template>
-	<div>
-		<section class="bg-[#1C1E53] py-[128px]">
-			<div class="container">
-				<div
-					class="max-md:border max-md:flex-col border-red-500 md:flex gap-x-8"
-				>
-					<div class="">
-						<h1
-							class="mb-7 text-[25px] md:text-[54px] max-w-[500px] mx-auto md:max-w-[582px] text-[white] font-bold leading-[42px] md:leading-[74px]"
-						>
-							Building stellar websites for early startups
-						</h1>
-						<p
-							class="max-md:max-w-[500px] max-md:mx-auto max-w-[460px] font-medium text-[16px] leading-7 text-[white]"
-						>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt.
-						</p>
-						<div
-							class="max-md:mb-[50px] flex gap-x-[55px] mt-[50px] items-center"
-						>
-							<button
-								class="max-md:ml-4 rounded-3xl font-medium border border-white-500 py-3 px-8 md:py-4 md:px-16 bg-[#FCD980]"
-							>
-								View our work
-							</button>
-							<div class=" ">
-								<p
-									class="max-md:pb-2 text-3 mt-2 md:text-[14px] font-medium leading-7 text-[white]"
-								>
-									View Pricing
-								</p>
-							</div>
-						</div>
-					</div>
-					<div>
-						<img class="max-md:w-[60%]" src="/images/intro.png" />
-					</div>
-				</div>
-			</div>
-		</section>
-	</div>
+  <div>
+    <section class="bg-[#1C1E53] py-[128px]">
+      <div class="container">
+        <div
+          class="max-md:border max-md:flex-col border-red-500 md:flex gap-x-8"
+        >
+          <div class="">
+            <h1
+              class="mb-7 text-[25px] md:text-[54px] max-w-[500px] mx-auto md:max-w-[582px] text-[white] font-bold leading-[42px] md:leading-[74px]"
+            >
+              Building stellar websites for early startups
+            </h1>
+            <p
+              class="max-md:max-w-[500px] max-md:mx-auto max-w-[460px] font-medium text-[16px] leading-7 text-[white]"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt.
+            </p>
+            <div
+              class="max-md:mb-[50px] flex gap-x-[55px] mt-[50px] items-center"
+            >
+              <button
+                class="max-md:ml-4 rounded-3xl font-medium border border-white-500 py-3 px-8 md:py-4 md:px-16 bg-[#FCD980]"
+              >
+                View our work
+              </button>
+              <div class=" ">
+                <p
+                  class="max-md:pb-2 text-3 mt-2 md:text-[14px] font-medium leading-7 text-[white] cursor-pointer"
+                >
+                  View Pricing
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img class="max-md:w-[60%]" src="/images/intro.png" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup></script>
