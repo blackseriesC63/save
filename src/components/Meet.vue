a263e121cb918f047e166be1d208c4d0fcc9cc80
blackseriesC6 <blackseries.n63@gmail.com>
2024-06-06 09:31:12 +0500

    about Final

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -1,90 +1,90 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 const myData = ref([
-	{
-		name: 'John Smith',
-		job: 'CEO',
-		img: [
-			'images/facebook.png',
-			'images/twitter.png',
-			'images/linkedin.png',
-			'images/radius.png',
-		],
-	},
-	{
-		name: 'Simon Adams',
-		job: 'CEO',
-		img: [
-			'images/facebook.png',
-			'images/twitter.png',
-			'images/linkedin.png',
-			'images/radius2.png',
-		],
-	},
-	{
-		name: 'Paul Jones',
-		job: 'Design Lead',
-		img: [
-			'images/facebook.png',
-			'images/twitter.png',
-			'images/linkedin.png',
-			'images/radius3.png',
-		],
-	},
-	{
-		name: 'Sara Hardin',
-		job: 'Project Manager',
-		img: [
-			'images/facebook.png',
-			'images/twitter.png',
-			'images/linkedin.png',
-			'images/radius.png',
-		],
-	},
-])
+  {
+    name: "John Smith",
+    job: "CEO",
+    img: [
+      "images/facebook.png",
+      "images/twitter.png",
+      "images/linkedin.png",
+      "images/radius.png",
+    ],
+  },
+  {
+    name: "Simon Adams",
+    job: "CEO",
+    img: [
+      "images/facebook.png",
+      "images/twitter.png",
+      "images/linkedin.png",
+      "images/radius2.png",
+    ],
+  },
+  {
+    name: "Paul Jones",
+    job: "Design Lead",
+    img: [
+      "images/facebook.png",
+      "images/twitter.png",
+      "images/linkedin.png",
+      "images/radius3.png",
+    ],
+  },
+  {
+    name: "Sara Hardin",
+    job: "Project Manager",
+    img: [
+      "images/facebook.png",
+      "images/twitter.png",
+      "images/linkedin.png",
+      "images/radius.png",
+    ],
+  },
+]);
 </script>
 
 <template>
-	<div class="bg-[#F4F6FC]">
-		<div class="container">
-			<div>
-				<h2
-					class="leading-[64px] text-[48px] font-bold text-center mb-[48px] pt-[128px]"
-				>
-					Meet our team
-				</h2>
-			</div>
-			<div class="md:grid grid-cols-4 gap-x-7 pb-[128px]">
-				<div v-for="(item, index) in myData" :key="index">
-					<div class="bg-[white] group hover::shadow py-[30px] mb-5">
-						<div
-							class="relative w-[168px] h-[168px] mx-auto rounded-full overflow-hidden"
-						>
-							<div
-								class="absolute left-0 -bottom-full duration-300 group-hover:bottom-0 w-full h-full bg-slate-900/70"
-							>
-								<div class="flex h-full justify-center items-end">
-									<div class="flex gap-3 pb-5">
-										<img :src="item.img[0]" alt="Facebook" />
-										<img :src="item.img[1]" alt="Twitter" />
-										<img :src="item.img[2]" alt="LinkedIn" />
-									</div>
-								</div>
-							</div>
-							<img :src="item.img[3]" alt="radius" class="w-full h-full" />
-						</div>
-						<p
-							class="text-[24px] leading-[36px] font-medium mt-[30px] text-center"
-						>
-							{{ item.name }}
-						</p>
-						<p class="text-center">{{ item.job }}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="bg-[#F4F6FC]">
+    <div class="container">
+      <div>
+        <h2
+          class="leading-[64px] text-[48px] font-bold text-center mb-[48px] pt-[128px]"
+        >
+          Meet our team
+        </h2>
+      </div>
+      <div class="md:grid grid-cols-4 gap-x-7 pb-[128px]">
+        <div v-for="(item, index) in myData" :key="index">
+          <div class="bg-[white] group hover::shadow py-[30px] mb-5">
+            <div
+              class="relative w-[168px] h-[168px] mx-auto rounded-full overflow-hidden"
+            >
+              <div
+                class="absolute left-0 -bottom-full duration-500 group-hover:bottom-0 w-full h-full bg-violet-500 bg-opacity-50"
+              >
+                <div class="flex h-full justify-center items-end">
+                  <div class="flex gap-3 pb-5">
+                    <img :src="item.img[0]" alt="Facebook" />
+                    <img :src="item.img[1]" alt="Twitter" />
+                    <img :src="item.img[2]" alt="LinkedIn" />
+                  </div>
+                </div>
+              </div>
+              <img :src="item.img[3]" alt="radius" class="w-full h-full" />
+            </div>
+            <p
+              class="text-[24px] leading-[36px] font-medium mt-[30px] text-center"
+            >
+              {{ item.name }}
+            </p>
+            <p class="text-center">{{ item.job }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
