cacd433d76b9ca1c5e9d4ae43a15fcc124583e0a
blackseriesC6 <blackseries.n63@gmail.com>
2024-06-07 21:05:07 +0500

    ohirgisi

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="container">
-    <div class="md:grid grid-cols-2 pt-[50px] pb-[26px] gap-4">
-      <div class="w-[595px]">
-        <h2 class="text-[18px] font-normal mb-2">About us</h2>
+  <div class="container mx-auto px-4">
+    <div class="md:grid grid-cols-2 pt-12 pb-2 gap-4">
+      <div class="w-full md:w-[595px]">
+        <h2 class="text-lg font-normal mb-2">About us</h2>
         <h2
-          class="text-[30px] md:text-[54px] font-bold leading-[77px] mb-[31px]"
+          class="text-2xl md:text-5xl font-bold leading-tight md:leading-[77px] mb-8"
         >
           Our designs solve problems
         </h2>
-        <p class="max-md:mb-4 max-md:mt-4 leading-7 text-[18px] font-normal">
+        <p class="mb-4 mt-4 leading-7 text-base md:text-lg font-normal">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam.
         </p>
       </div>
-      <div>
-        <img src="/images/about.png" />
+      <div class="flex justify-center">
+        <img src="/images/about.png" class="w-full max-w-md md:max-w-full" />
       </div>
     </div>
   </div>
@@ -25,4 +25,8 @@
 const data = 12;
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  @apply max-w-screen-lg;
+}
+</style>
